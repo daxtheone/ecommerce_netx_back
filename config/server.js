94344c35699182936cebb3dba91c3aad762518e1
daxtheone <danielricomaya@gmail.com>
2021-06-02 22:35:52 -0400

    actualizacion

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7e3114e544dbbf97a8cf685dd73edcf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0fb92905-5011-4c77-a3e8-214569999813'
+};
